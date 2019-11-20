b2b4dc6a78bfda694ee6cfd872b1ffbffaf3e2d7
mlmoreto <marialauramoreto@hotmail.com>
2019-11-20 12:24:46 -0300

    Passo 9 - No arquivo MainActivity.java adionado ao metodo onCreate

--- app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
@@ -21,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Após a criação da tela
+        geradorRandomico = new Random(System.currentTimeMillis());
+
+        // Recuperando referência para o resultadoTextView do arquivo de layout
+        resultadoTextView = findViewById(R.id.resultadoTextView);
+
+        // Recuperando referência para o jogarDadoButton do arquivo de layout
+        jogarDadoButton = findViewById(R.id.jogarDadoButton);
+        jogarDadoButton.setOnClickListener(this);
     }
 }
