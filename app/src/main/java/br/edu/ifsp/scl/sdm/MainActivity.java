097edac250a1b26bde757d7b13df8909fde7b2fb
mlmoreto <marialauramoreto@hotmail.com>
2019-11-20 13:28:57 -0300

    Parte 2 - Passo 4 - Adição de atributo ImageView na MainActivity.java

--- app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     // Componentes visuais
     private TextView resultadoTextView;
     private Button jogarDadoButton;
+
+    private ImageView resultadoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
