f9aa764214ba53d8f08877038b0d76b3e9072728
mlmoreto <marialauramoreto@hotmail.com>
2019-11-20 13:53:39 -0300

    Parte 4 - Passo 5 - Alteração do onClick

--- app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
@@ -58,29 +58,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (view.getId() == R.id.jogarDadoButton) {
 
             // Recuperando o número de dados selecionados
-            int numDados = Integer.parseInt(
-                    numDadosSpinner.getSelectedItem().toString());
+            int numDados =
+                    Integer.parseInt(numDadosSpinner.getSelectedItem().toString());
 
             // String que armazena números sorteados
             String resultadoText = getString(R.string.txtFaceSorteada);
 
-            // Visibilidade do resultado2ImageView de acordo com número de dados
-            if (numDados == 2) {
-                resultado2ImageView.setVisibility(View.VISIBLE);
+            int numFaces;
+
+            try {
+                numFaces = Integer.parseInt(numFacesEditText.getText().toString());
+            } catch (NumberFormatException e) {
+                // Caso usuário não digite nenhum número de faces
+                numFaces = 6;
+            }
+            if (numFaces > 6) {
+                resultadoImageView.setVisibility(View.GONE);
+                resultado2ImageView.setVisibility(View.GONE);
             }
             else {
-                resultado2ImageView.setVisibility(View.GONE);
-                resultadoText = getString(R.string.txtFaceSorteada);
+                resultadoImageView.setVisibility(View.VISIBLE);
+                // Visibilidade do resultado2ImageView de acordo com número de dados
+                if (numDados == 2) {
+                    resultado2ImageView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    resultado2ImageView.setVisibility(View.GONE);
+                    resultadoText = getString(R.string.txtFaceSorteada);
+                }
             }
+
             // Sorteando números de acordo com número de dados
             for (int i = 1; i <= numDados; i++) {
-                int resultado = geradorRandomico.nextInt(6) + 1;
+                int resultado = geradorRandomico.nextInt(numFaces) + 1;
                 resultadoText += resultado + ", ";
                 ImageView iv = (i == 1) ? resultadoImageView : resultado2ImageView;
                 setImageResource(iv, resultado);
             }
-            resultadoTextView.setText(
-                    resultadoText.substring(0, resultadoText.lastIndexOf(',')));
+
+            resultadoTextView.setText(resultadoText.substring(0,
+                    resultadoText.lastIndexOf(',')));
         }
     }
 
