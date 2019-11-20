e9604575ff4ac9d7dc41242cd0cd05b5ad23bf97
mlmoreto <marialauramoreto@hotmail.com>
2019-11-20 12:23:26 -0300

    Passo 8 - Adição de atributos na MainAcitivy.java

--- app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
@@ -3,8 +3,19 @@ package br.edu.ifsp.scl.sdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Random usado para simular o lançamento do dado
+    private Random geradorRandomico;
+
+    // Componentes visuais
+    private TextView resultadoTextView;
+    private Button jogarDadoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
